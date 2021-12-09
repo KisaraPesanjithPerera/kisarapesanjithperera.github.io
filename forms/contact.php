1e5f44ade6bdf78aed77ac28beb7a7fd0ea046ff
Kɪꜱᴀʀᴀ Pᴇꜱᴀɴᴊɪᴛʜ Pᴇʀᴇʀᴀ〽️ <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-12-09 05:37:02 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'senu@cxboy.cf';
+  $receiving_email_address = 'kpwithworks@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
